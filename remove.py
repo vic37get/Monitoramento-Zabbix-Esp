9158ee49d41525ddb2960b22e8e7145059573025
Victor <victorribeiro172@gmail.com>
2022-10-28 15:54:58 -0300

    remoções

--- remove.py
+++
@@ -1,9 +0,0 @@
-with open('codigo.txt') as f:
-    dados = f.readlines()
-
-for linha in range(len(dados)):
-    dados[linha] = dados[linha][1:]
-
-with open('codigo.txt', 'w') as f:
-    for i in dados:
-        f.write(i)
